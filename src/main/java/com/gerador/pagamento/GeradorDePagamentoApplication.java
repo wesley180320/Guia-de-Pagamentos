d1f01c1a4cdc06f282d94f1cc3da621afd9b8a17
wesley180320 <wesleyestevao333@gmail.com>
2025-09-04 11:13:20 -0400

    Remocao de codigo de teste comentado

--- src/main/java/com/gerador/pagamento/GeradorDePagamentoApplication.java
+++ src/main/java/com/gerador/pagamento/GeradorDePagamentoApplication.java
@@ -18,25 +18,6 @@ public class GeradorDePagamentoApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-//		String pastaProjeto = "pdfs"; // pasta dentro do projeto
-//		File dir = new File(pastaProjeto);
-//		if (!dir.exists()) {
-//			dir.mkdirs(); // cria a pasta se não existir
-//		}
-//		String caminho = pastaProjeto + "/guia.pdf";
-//
-//		GerarGuia.gerarGuiaPdf(
-//				caminho,
-//				"Wesley",
-//				"123.456.789-00",
-//				"Rua Teste, 123",
-//				new BigDecimal("150.00"),
-//				LocalDate.now().plusDays(5),
-//				"chave-pix-exemplo",
-//				"Recebedor Teste",
-//				"São Paulo"
-//		);
-//		System.out.println("PDF gerado com sucesso!");
 	}
 
 }
