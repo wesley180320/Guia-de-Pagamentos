3a1e5fb32b5e512169f6c0b73d43ae4349a49173
wesleyestevao <wesleyestevao333@gmail.com>
2026-04-12 10:22:19 -0400

    Adicionando SRP solid, separando responsabilidades.

--- src/main/java/com/gerador/pagamento/controller/PagamentoController.java
+++ src/main/java/com/gerador/pagamento/controller/PagamentoController.java
@@ -2,7 +2,8 @@ package com.gerador.pagamento.controller;
 
 import com.gerador.pagamento.DTO.PagamentoDTO;
 import com.gerador.pagamento.model.Cliente;
-import com.gerador.pagamento.service.ClienteService;
+import com.gerador.pagamento.service.PdfService;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -17,11 +18,11 @@ import javax.validation.Valid;
 @RequestMapping("/pagamento")
 public class PagamentoController {
     @Autowired
-    private ClienteService clienteService;
+    private PdfService pdfService;
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
-    public ResponseEntity<byte[]> save(@RequestBody @Valid PagamentoDTO pagamentoDTO, @AuthenticationPrincipal Cliente cliente) throws Exception {
-        byte[] pdfBytes = clienteService.gerarPdf(pagamentoDTO, cliente);
+    public ResponseEntity<byte[]> save(@RequestBody @Valid PagamentoDTO pagamentoDTO, @Parameter(hidden = true)@AuthenticationPrincipal Cliente cliente) throws Exception {
+        byte[] pdfBytes = pdfService.gerarPdf(pagamentoDTO, cliente);
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=guia.pdf")
                 .contentType(MediaType.APPLICATION_PDF)
--- src/main/java/com/gerador/pagamento/service/ClienteService.java
+++ src/main/java/com/gerador/pagamento/service/ClienteService.java
@@ -1,20 +1,14 @@
 package com.gerador.pagamento.service;
 
 import com.gerador.pagamento.DTO.LoginDTO;
-import com.gerador.pagamento.DTO.PagamentoDTO;
 import com.gerador.pagamento.exception.ClienteException;
 import com.gerador.pagamento.model.Cliente;
 import com.gerador.pagamento.model.Recebedor;
 import com.gerador.pagamento.repository.ClienteRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import javax.validation.ConstraintViolationException;
-import java.math.BigDecimal;
-import java.time.LocalDate;
 
 @Service
 public class ClienteService {
@@ -24,41 +18,6 @@ public class ClienteService {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
-
-    public byte[] gerarPdf(PagamentoDTO pagamentoDTO, Cliente cliente) {
-
-        Cliente clienteLogado = validarClienteLogado(cliente);
-        Recebedor recebedor = new Recebedor();
-        BeanUtils.copyProperties(pagamentoDTO, recebedor);
-        clienteLogado.getRecebedor().setCidade(recebedor.getCidade());
-        clienteLogado.getRecebedor().setNome(recebedor.getNome());
-
-        byte[] pdfByte = GerarGuia.gerarGuiaPdf(
-                recebedor.getNome(),
-                clienteLogado.getCpf(),
-                clienteLogado.getEndereco(),
-                new BigDecimal(String.valueOf(recebedor.getValor())),
-                LocalDate.now().plusDays(5),
-                "chave-pix-exemplo",
-                clienteLogado.getRecebedor().getNome(),
-                clienteLogado.getRecebedor().getCidade());
-
-        if (pdfByte == null) {
-            throw new ClienteException("Erro ao gerar pdf");
-        }
-
-        try {
-            clienteLogado.getRecebedor().setCliente(clienteLogado);
-            clienteRepository.save(clienteLogado);
-            return pdfByte;
-        } catch (ConstraintViolationException e) {
-            throw new ClienteException("Erro ao salvar o cliente: violação de constraint", e);
-        } catch (DataAccessException e) {
-            throw new ClienteException("Erro ao salvar o cliente no banco de dados", e);
-        } catch (Exception e) {
-            throw new ClienteException("Erro inesperado ao salvar o cliente", e);
-        }
-    }
 
     public void salvarCliente(LoginDTO loginDTO) {
         validarCliente(loginDTO);
@@ -75,13 +34,5 @@ public class ClienteService {
         if (clienteExistente != null) {
             throw new ClienteException("Erro ao salvar o cliente: CPF já existe");
         }
-    }
-
-    private Cliente validarClienteLogado(Cliente cliente) {
-        Cliente clienteLogado = clienteRepository.findByClienteComRecebedor(cliente.getCpf());
-        if (clienteLogado == null) {
-            throw new ClienteException("Cliente não pode ser nulo");
-        }
-        return clienteLogado;
     }
 }
